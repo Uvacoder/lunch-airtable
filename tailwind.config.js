fdf8e094b238f8e1d5f7349aaa66c2f15478e68a
David Leininger <davidbleininger@gmail.com>
2020-03-16 23:43:08 -0500

    add dark-mode variants

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,54 @@
+const plugin = require('tailwindcss/plugin')
+
 module.exports = {
   theme: {
+    darkSelector: 'dark-mode',
     extend: {},
   },
-  variants: {},
-  plugins: [],
+  variants: {
+    backgroundColor: ['responsive', 'hover', 'focus', 'dark', 'dark-hover', 'dark-group-hover'],
+    borderColor: ['responsive', 'hover', 'focus', 'dark', 'dark-focus', 'dark-focus-within'],
+    textColor: ['responsive', 'hover', 'focus', 'dark', 'dark-hover', 'dark-active']
+  },
+  plugins: [
+    plugin(function({ addVariant, theme, e }) {
+      const darkSelector = theme('darkSelector');
+
+      addVariant('dark', ({modifySelectors, separator}) => {
+        modifySelectors(({className}) => {
+          return `.${darkSelector} .${e(`dark${separator}${className}`)}`
+        })
+      })
+
+      addVariant('dark-hover', ({modifySelectors, separator}) => {
+        modifySelectors(({className}) => {
+          return `.${darkSelector} .${e(`dark-hover${separator}${className}`)}:hover`
+        })
+      })
+
+      addVariant('dark-focus', ({modifySelectors, separator}) => {
+        modifySelectors(({className}) => {
+          return `.${darkSelector} .${e(`dark-focus${separator}${className}`)}:focus`
+        })
+      })
+
+      addVariant('dark-active', ({modifySelectors, separator}) => {
+        modifySelectors(({className}) => {
+          return `.${darkSelector} .${e(`dark-active${separator}${className}`)}:active`
+        })
+      })
+
+      addVariant('dark-group-hover', ({modifySelectors, separator}) => {
+        modifySelectors(({className}) => {
+          return `.${darkSelector} .group:hover .${e(`dark-group-hover${separator}${className}`)}`
+        })
+      })
+
+      addVariant('dark-focus-within', ({modifySelectors, separator}) => {
+        modifySelectors(({className}) => {
+          return `.${darkSelector} .${e(`dark-focus-within${separator}${className}`)}:focus-within`
+        })
+      })
+    })
+  ],
 }
