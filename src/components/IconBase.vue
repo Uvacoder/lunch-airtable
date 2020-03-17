227a29965fab13e42a1edf74f607aba6f8816272
David Leininger <davidbleininger@gmail.com>
2020-03-16 23:43:40 -0500

    Icons

---
+++ src/components/IconBase.vue
@@ -0,0 +1,44 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg"
+    :width="width"
+    :height="height"
+    viewBox="0 0 24 24"
+    :aria-labelledby="iconName"
+    role="presentation"
+  >
+    <title
+      :id="iconName"
+      lang="en"
+    >{{ iconName }} icon</title>
+    <g :fill="fillColor">
+      <slot />
+    </g>
+  </svg>
+</template>
+
+<script>
+export default {
+  props: {
+    iconName: {
+      type: String,
+      default: 'box'
+    },
+    width: {
+      type: [Number, String],
+      default: 18
+    },
+    height: {
+      type: [Number, String],
+      default: 18
+    },
+    fillColor: {
+      type: String,
+      default: 'currentColor'
+    },
+    strokeColor: {
+      type: String,
+      default: 'none'
+    },
+  }
+}
+</script>
