13c886a4761f869e3da1f7db2c743e850dd748a8
David Leininger <davidleininger@hey.com>
2022-03-26 23:06:56 -0500

    Fixed scroll and logic. Lands on Random.

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,24 +1,24 @@
 <template>
   <Layout>
 
-    <h1>Lunch Time!</h1>
+    <h1>Lunch Time<span v-if="selectedRestaurant"> at {{ selectedRestaurant.name }}!</span></h1>
 
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores
+    <p class="mb-6">
+      Makint decisions is hard. Like really hard. If the team can't decide where to lunch, just let the lunch time slot machine do it.
     </p>
 
     <div class="bg-green-100 h-32 overflow-hidden w-full border-teal-500 border-2 relative">
-      <ul class="slot-list absolute top-0 w-full" :class="{'running': isRunning}">
-        <li class="flex items-center justify-center bg-white p-4 h-32">
-          <h2 class="text-6xl font-bold leading-none text-teal-200 cursor-pointer" @click="selectRestaurant">What's for Lunch?</h2>
-        </li>
+      <component is="ul" ref="slotz" class="slot-list absolute top-0 w-full" :class="{'running': isRunning}">
         <li v-for="(r, index) in slots" :key="`${r.id}-${index}`" class="flex items-center justify-center bg-white p-4 h-32">
-          <h2 class="text-6xl font-bold leading-none">{{ r.name }}</h2>
+          <p class="text-6xl font-bold leading-none">{{ r.name }}</p>
         </li>
-      </ul>
+      </component>
+      <div v-if="!slots.length" class="flex items-center justify-center bg-white p-4 h-32 relative">
+        <p class="text-6xl font-bold leading-none text-teal-200 cursor-pointer" @click="runSlots">What's for Lunch?</p>
+      </div>
     </div>
 
-    <button @click="selectRestaurant">I'm Hungry</button>
+    <button @click="runSlots" :disabled="isRunning">I'm Hungry</button>
 
   </Layout>
 </template>
@@ -59,6 +59,9 @@ export default {
   data() {
     return {
       isRunning: false,
+      selectedIndex: 0,
+      selectedRestaurant: null,
+      slots: [],
     }
   },
   computed: {
@@ -82,20 +85,26 @@ export default {
           })
         }
       })
-    },
-    slots() {
-      return [...this.inflatedRestaurants, ...this.inflatedRestaurants]
     }
   },
   methods: {
-    selectRestaurant() {
-      const length = this.inflatedRestaurants.length
-      const selected = Math.floor(Math.random() * (length - length*2 + 1) + length)
-      this.isRunning = !this.isRunning
-      return selected
-    },
     runSlots() {
-      const selected = selectRestaurant()
+      const lastSelected = this.selectedRestaurant
+      let slotsArray = this.inflatedRestaurants
+      if (lastSelected) slotsArray.splice(slotsArray.indexOf(lastSelected), 1)
+      const length = slotsArray.length
+      const selectedIndex = Math.floor(Math.random() * length + 1)
+      const trimmedSlotsArray = slotsArray.slice(0, selectedIndex + 1)
+      let scrollSlotsArray = [...slotsArray, ...slotsArray]
+      if (lastSelected) scrollSlotsArray = [lastSelected, ...scrollSlotsArray]
+      this.slots = [...scrollSlotsArray, ...trimmedSlotsArray]
+      this.selectedRestaurant = null
+      this.isRunning = true
+      setTimeout(() => {
+        this.selectedRestaurant = this.inflatedRestaurants[selectedIndex]
+        this.slots = [this.selectedRestaurant]
+        this.isRunning = false
+      }, 5000)
     }
   }
 }
@@ -103,9 +112,13 @@ export default {
 
 <style>
 .slot-list {
-  transition: 15s ease;
+  transition: 0s;
 }
 .running {
-  transform: translateY(calc(-100% + 16rem));
+  transform: translateY(calc(-100% + 8rem));
+  transition: 5s ease-out;
+}
+:disabled {
+  background: #e1e3e8;
 }
 </style>
