f3c7c8d47baeaf012de79ff4d5e98f1f1f110f44
David Leininger <davidleininger@hey.com>
2022-04-16 00:51:36 -0500

    I'm good at typing

--- src/main.js
+++ src/main.js
@@ -9,26 +9,44 @@ export default function (Vue, { router, head, isClient }) {
   Vue.component('Layout', DefaultLayout)
 
   head.meta.push({
+    key: 'og:title',
+    name: 'og:title',
+    content: `Lunch Time Slots`,
+  })
+  head.meta.push({
+    key: 'og:url',
+    name: 'og:url',
+    content: `https://lunch-time.netlify.app`,
+  })
+  head.meta.push({
     key: 'og:description',
     name: 'og:description',
-    content: `Lunchtime Slot Machine for picking team lunches`,
+    content: `Lunch time Slot Machine for picking team lunches`,
   })
-
   head.meta.push({
     key: 'og:image',
     name: 'og:image',
-    content: `https://luncht-time.netlify.app/lunchtime-og-img.jpg`,
+    content: `https://lunch-time.netlify.app/lunchtime-og-img.jpg`,
   })
 
+  head.meta.push({
+    key: 'twitter:card',
+    name: 'twitter:card',
+    content: `summary_large_image`,
+  })
+  head.meta.push({
+    key: 'twitter:title',
+    name: 'twitter:title',
+    content: `Lunch Time Slots`,
+  })
   head.meta.push({
     key: 'twitter:description',
     name: 'twitter:description',
-    content: `Lunchtime Slot Machine for picking team lunches`,
+    content: `Lunch time Slot Machine for picking team lunches`,
   })
-
   head.meta.push({
     key: 'twitter:image',
     name: 'twitter:image',
-    content: `https://luncht-time.netlify.app/lunchtime-og-img.jpg`,
+    content: `https://lunch-time.netlify.app/lunchtime-og-img.jpg`,
   })
 }
